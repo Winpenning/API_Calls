5c0e73df2160f67ddf12a6cdeab09ec929985e2d
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-07-29 08:21:00 -0300

    fazendo um ajuste

--- API_Calls/app/src/main/java/com/example/api_calls/WheatherPage.kt
+++ API_Calls/app/src/main/java/com/example/api_calls/WheatherPage.kt
@@ -38,59 +38,70 @@ import com.example.api_calls.api.NetworkResponse
 import com.example.api_calls.api.WeatherModel
 
 @Composable
-fun WheatherPage(viewModel: WeatherViewModel) {
+fun WeatherPage(viewModel: WeatherViewModel) {
+
     var city by remember {
         mutableStateOf("")
     }
-    val weatherResult = viewModel.weatherResult.observeAsState()
-    val keyboardcontroller = LocalSoftwareKeyboardController.current
 
-    Column (
+    val weatherResult = viewModel.weatherResult.observeAsState()
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Row (
+    ) {
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly
-        ){
+        ) {
             OutlinedTextField(
                 modifier = Modifier.weight(1f),
                 value = city,
                 onValueChange = {
-                city = it
-            },
+                    city = it
+                },
                 label = {
-                Text(text = "Search for any location.")
-            })
+                    Text(text = "Search for any location")
+                }
+            )
             IconButton(onClick = {
                 viewModel.getData(city)
-                keyboardcontroller?.hide()
+                keyboardController?.hide()
             }) {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "Search for any location.")
+                Icon(imageVector = Icons.Default.Search,
+                    contentDescription = "Search for any location"
+                )
             }
+
         }
+
         when(val result = weatherResult.value){
             is NetworkResponse.Error -> {
                 Text(text = result.message)
             }
-            is NetworkResponse.Loading -> {
+            NetworkResponse.Loading -> {
                 CircularProgressIndicator()
             }
             is NetworkResponse.Success -> {
-                Text(text = result.data.toString())
+                WeatherDetails(data = result.data)
             }
-            null -> {}
+            null -> {
+            }
         }
+
     }
 }
 
+
 @Composable
-fun WeatherDetails(data: WeatherModel) {
+fun WeatherDetails(data : WeatherModel) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -101,69 +112,78 @@ fun WeatherDetails(data: WeatherModel) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.Bottom
-            ){
-            Icon(imageVector = Icons.Default.LocationOn,
-                contentDescription = "Location Icon",
-                modifier = Modifier.size(40.dp))
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location icon",
+                modifier = Modifier.size(40.dp)
+            )
             Text(text = data.location.name, fontSize = 30.sp)
             Spacer(modifier = Modifier.width(8.dp))
             Text(text = data.location.country, fontSize = 18.sp, color = Color.Gray)
         }
+
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = "${data.current.temp_c} ° c",
-            fontSize = 55.sp,
+            text = " ${data.current.temp_c} ° c",
+            fontSize = 56.sp,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
         )
-    }
-    AsyncImage(
-        modifier = Modifier.size(160.dp),
-        model ="https:${data.current.condition.icon}".replace("64x64","128x128"),
-        contentDescription = "Condition icon")
-    Text(
-        text = "${data.current.condition.text}",
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
-        color = Color.Gray
-    )
-    Spacer(modifier = Modifier.height(16.dp))
-    Card{
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ){
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ){
-                WeatherKeyVal("Humidity", data.current.humidity)
-                WeatherKeyVal("Wind Speed", "${data.current.wind_kph} km/h")
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ){
-                WeatherKeyVal("UV", data.current.uv)
-                WeatherKeyVal("Participation", "${data.current.precip_mm} mm")
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ){
-                WeatherKeyVal("Local Time", data.location.localtime.split(" ")[1])
-                WeatherKeyVal("Local Date", data.location.localtime.split(" ")[0])
+
+        AsyncImage(
+            modifier = Modifier.size(160.dp),
+            model = "https:${data.current.condition.icon}".replace("64x64","128x128"),
+            contentDescription = "Condition icon"
+        )
+        Text(
+            text = data.current.condition.text,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Card {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Humidity",data.current.humidity)
+                    WeatherKeyVal("Wind Speed",data.current.wind_kph+" km/h")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("UV",data.current.uv)
+                    WeatherKeyVal("Participation",data.current.precip_mm+" mm")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Local Time",data.location.localtime.split(" ")[1])
+                    WeatherKeyVal("Local Date",data.location.localtime.split(" ")[0])
+                }
             }
         }
+
+
+
     }
+
 }
 
 @Composable
-fun WeatherKeyVal(key: String, value: String) {
+fun WeatherKeyVal(key : String, value : String) {
     Column(
         modifier = Modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text =value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        Text(text = key,fontWeight = FontWeight.SemiBold, color = Color.Gray)
+        Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text(text = key, fontWeight = FontWeight.SemiBold, color = Color.Gray)
     }
 }
